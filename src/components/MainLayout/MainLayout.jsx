ee856d8f8d104ab7a856ba2ed628012dbdb50732
Федоров Евгений Серапионович <fedorov_es@sovts.local>
2025-07-31 13:29:47 +0300

    Added MainLayout.jsx, Routing, Global css

---
+++ src/components/MainLayout/MainLayout.jsx
@@ -0,0 +1,21 @@
+import { Outlet } from "react-router-dom";
+import style from "./MainLayout.module.css";
+
+export const MainLayout = () => {
+  const currentYear = new Date().getFullYear();
+
+  return (
+    <div className={style.mainLayout}>
+      <header>head</header>
+      <div className={style.mainWrapper}>
+        <main className={style.main}>
+          <Outlet />
+        </main>
+        <footer className={style.footer}>
+          React Question Cards Application | {currentYear} <br />
+          by Fedorov Evgeniy
+        </footer>
+      </div>
+    </div>
+  );
+};
