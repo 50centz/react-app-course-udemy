5b75565c1c608c7f87d5965139fab9fb56f9224c
Федоров Евгений Серапионович <fedorov_es@sovts.local>
2025-08-21 12:02:58 +0300

    Adding new question

--- src/pages/AddQuestionPage/AddQuestionPage.jsx
+++ src/pages/AddQuestionPage/AddQuestionPage.jsx
@@ -1,16 +1,54 @@
+import { useActionState } from "react";
 import { Button } from "../../components/Button/Button.jsx";
+import { delayFn } from "../../helpers/delayFn.js";
+import { toast } from "react-toastify";
+import { API_URL } from "../../constants/index.js";
 import style from "./AddQuestionPage.module.css";
 
+const createCardAction = async (_prevState, formData) => {
+  try {
+    await delayFn();
+
+    const newQuestion = Object.fromEntries(formData);
+    const resources = newQuestion.resources.trim();
+    const isClearForm = newQuestion.clearForm; // formData.get("question")
+
+    const response = await fetch(`${API_URL}/react`, {
+      method: "POST",
+      body: JSON.stringify({
+        question: newQuestion.question,
+        answer: newQuestion.answer,
+        description: newQuestion.description,
+        resources: resources.length ? resources.split(",") : [],
+        level: Number(newQuestion.level),
+        completed: false,
+        editDate: undefined,
+      }),
+    });
+
+    const question = response.json();
+    toast.success("New question is successfuly created");
+
+    return isClearForm ? {} : question;
+  } catch (error) {
+    toast.error(error.message);
+  }
+};
+
 export const AddQuestionPage = () => {
+  const [formState, formAction, isPending] = useActionState(createCardAction, {
+    clearForm: false,
+  });
+
   return (
     <>
       <h1 className={style.formTitle}>Add new question</h1>
       <div className={style.formContainer}>
-        <form action="" className={style.form}>
+        <form action={formAction} className={style.form}>
           <div className={style.formControl}>
             <label htmlFor="questionField">Question: </label>
             <textarea
-              defaultValue={""}
+              defaultValue={formState.question}
               name="question"
               id="questionField"
               cols="30"
@@ -22,19 +60,7 @@ export const AddQuestionPage = () => {
           <div className={style.formControl}>
             <label htmlFor="answerField">Short Answer: </label>
             <textarea
-              defaultValue={""}
-              name="answer"
-              id="answerField"
-              cols="30"
-              rows="2"
-              required
-              placeholder="PLease enter a short answer"
-            ></textarea>
-          </div>
-          <div className={style.formControl}>
-            <label htmlFor="answerField">Short Answer: </label>
-            <textarea
-              defaultValue={""}
+              defaultValue={formState.answer}
               name="answer"
               id="answerField"
               cols="30"
@@ -46,7 +72,7 @@ export const AddQuestionPage = () => {
           <div className={style.formControl}>
             <label htmlFor="descriptionField">Description: </label>
             <textarea
-              defaultValue={""}
+              defaultValue={formState.description}
               name="description"
               id="descriptionField"
               cols="30"
@@ -58,7 +84,7 @@ export const AddQuestionPage = () => {
           <div className={style.formControl}>
             <label htmlFor="resourcesField">Resources: </label>
             <textarea
-              defaultValue={""}
+              defaultValue={formState.resources}
               name="resources"
               id="resourcesField"
               cols="30"
@@ -69,7 +95,7 @@ export const AddQuestionPage = () => {
           </div>
           <div className={style.formControl}>
             <label htmlFor="levelField">Level: </label>
-            <select name="level" id="levelField" defaultValue={""}>
+            <select name="level" id="levelField" defaultValue={formState.level}>
               <option disabled>Question level</option>
               <hr />
               <option value="1">1 - easiest</option>
@@ -84,12 +110,12 @@ export const AddQuestionPage = () => {
               type="checkbox"
               name="clearForm"
               id="clearFormField"
-              defaultValue={true}
+              defaultChecked={formState.clearForm}
             />
             <span>clear form after submitting ?</span>
           </label>
 
-          <Button>Add question</Button>
+          <Button isDisabled={isPending}>Add question</Button>
         </form>
       </div>
     </>
